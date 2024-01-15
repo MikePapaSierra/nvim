73b1274204bc643e9f73cf504fb989032fcd0446
kamikazestar <8329080+kamikazestar@users.noreply.github.com>
2024-01-15 22:51:17 +0100

    feat(obsidian.lua): Add Obsidian plugin configuration
    Enable Obsidian plugin with the default plugin configuration. Adding Markdown support in to the Treesitter configuration.

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,7 +3,7 @@ return {
 	build = ':TSUpdate',
 	config = function()
 		require('nvim-treesitter.configs').setup({
-			ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'go', 'fish', 'dockerfile', 'bash' },
+			ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'go', 'fish', 'dockerfile', 'bash', 'markdown', 'markdown_inline' },
 
 			sync_install = false,
 
