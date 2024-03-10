edd0a7956b23f9d0b79e52eccab113d67efc45f6
kamikazestar <8329080+kamikazestar@users.noreply.github.com>
2024-03-10 18:34:40 +0100

    feat(telescope): Add telescope-ui-select plugin configuration

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,5 +8,18 @@ return {
 		'nvim-telescope/telescope-fzf-native.nvim',
 		build = 'make',
 		cond = vim.fn.executable 'make' == 1
+	},
+	{
+		'nvim-telescope/telescope-ui-select.nvim',
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown {}
+					}
+				}
+			})
+			require("telescope").load_extension("ui-select")
+		end
 	}
 }
