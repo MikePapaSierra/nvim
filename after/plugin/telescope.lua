367510b080c5da26fafcaa25c085b88f6caa7c6f
MikePapaSierra <8329080+MikePapaSierra@users.noreply.github.com>
2024-03-30 22:38:53 +0100

    feat(telescope): update keymaps for telescope plugin

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -24,12 +24,23 @@ vim.keymap.set('n', '<leader>/', function()
 		previewer = false,
 	})
 end, { desc = '[/] Fuzzily search in curernt buffer]' })
-
-vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
+vim.keymap.set("n", "<leader>ff", function()
+	require("telescope.builtin").find_files({ hidden = true })
+end, { desc = "[F]ind [F]iles" })
 vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
 vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[F]ind current [W]ord' })
 vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[F]ind by [G]rep' })
 vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
-vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>fS', require('telescope.builtin').git_status, { desc = '' })
+vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers, { desc = "[F]ind over [B]uffers" })
+vim.keymap.set('n', '<leader>fS', require('telescope.builtin').git_status, { desc = '[F]ind Git [S]tatus' })
 vim.keymap.set("n", "<Leader>fn", "<CMD>lua require('telescope').extensions.notify.notify()<CR>", silent)
+vim.keymap.set("n", "<leader>fc", function ()
+	require("telescope.builtin").commands(require("telescope.themes").get_dropdown({
+		preview = false,
+	}))
+end, { desc = "[F]ind [C]ommands" })
+vim.keymap.set("n", "<leader>ss", function()
+	require("telescope.builtin").spell_suggest(require("telescope.themes").get_dropdown({
+		preview = false,
+	}))
+end, { desc = "[S]pell [S]uggest" })
