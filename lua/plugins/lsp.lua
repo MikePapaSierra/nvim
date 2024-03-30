f729db38b27c0628533ca1d6603992efae6c0e50
kamikazestar <8329080+kamikazestar@users.noreply.github.com>
2024-03-30 22:40:17 +0100

    feat(lsp): moved none-ls to single lsp file

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -24,6 +24,7 @@ return {
 	},
 	{
 		'neovim/nvim-lspconfig',
+		event = { "BufReadPost" },
 		config = function()
 			local capabilities = require('cmp_nvim_lsp').default_cpapbilities
 			local lspconfig = require('lspconfig')
@@ -33,7 +34,33 @@ return {
 			lspconfig.pyright.setup({
 				capabilities = capabilities
 			})
+			lspconfig.ansiblels.setup({
+				capabilities = capabilities
+			})
 		end
 	},
-	'folke/neodev.nvim',
+	{
+		'nvimtools/none-ls.nvim',
+		config = function()
+		local null_ls = require('null-ls')
+
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
+				-- TODO: [null-ls] failed to load builtin ansible_lint for method formatting; please check your config
+				-- [lspconfig] Cannot access configuration for ansible. Ensure this server is listed in `server_configurations.md` or added as a custom server.
+				null_ls.builtins.formatting.ansible_lint,
+			}
+		})
+		vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
+		end
+	},
+	{
+		'folke/neodev.nvim',
+		config = function()
+			require('neodev').setup()
+		end
+	},
 }
