3dae26825eab964ca8466509afcc45b84f5c3448
kamikazestar <8329080+kamikazestar@users.noreply.github.com>
2024-03-31 10:45:02 +0200

    feat(completion): update configuration

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -11,15 +11,29 @@ return {
 			"saadparwaiz1/cmp_luasnip",
 			"rafamadriz/friendly-snippets",
 		},
+		config = function ()
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end
 	},
 	{
 		"hrsh7th/nvim-cmp",
 		dependencies = {
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-emoji",
+			"onsails/lspkind-nvim",
+			"windwp/nvim-autopairs",
 		},
 		config = function()
 			local cmp = require("cmp")
-			-- require("luasnip.loadres.from_vscode").lazy_load()
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local lspkind = require("lspkind")
+			local luasnip = require("luasnip")
+
+			require("nvim-autopairs").setup()
+
+			-- Integrate nvim-autopairs with cmp
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
 			cmp.setup({
 				snippet = {
@@ -32,20 +46,77 @@ return {
 					documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
+					["Tab"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["S-Tab"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<C-d>"] = cmp.mapping.scroll_docs(-4),
+					["<C-u>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete({}),
+					["<C-c>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 				}),
 				sources = cmp.config.sources({
 					{ name = 'nvim_lsp' },
-					{ name = "luasnip" },
-				}, {
-					{ name = "buffer" },
-				}, {
-					{ name = "emoji" },
+					{ name = 'buffer', max_item_count = 5 },
+					{ name = 'copilot' },
+					{ name = 'path', max_item_count = 3 },
+					{ name = "luasnip", max_item_count = 3 },
+					{ name = 'emoji' },
 				}),
+				formatting = {
+					expandable_indicator = true,
+					format = lspkind.cmp_format({
+						mode = "symbol_text",
+						maxwidth = 50,
+						ellipsis_char = "…",
+						symbol_map = {
+							Text = "",
+							Method = "",
+							Function = "",
+							Constructor = "",
+							Field = "ﰠ",
+							Variable = "",
+							Class = "ﴯ",
+							Interface = "",
+							Module = "",
+							Property = "ﰠ",
+							Unit = "塞",
+							Value = "",
+							Enum = "",
+							Keyword = "",
+							Snippet = "",
+							Color = "",
+							File = "",
+							Reference = "",
+							Folder = "",
+							EnumMember = "",
+							Constant = "",
+							Struct = "פּ",
+							Event = "",
+							Operator = "",
+							TypeParameter = "",
+							Copilot = "",
+						},
+					})
+				},
+				experimental = {
+					ghost_text = true,
+				},
 			})
 		end,
 	},
