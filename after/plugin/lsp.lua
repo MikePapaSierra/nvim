90fb711943548eebbfb474ae1712845e242bddb9
MikePapaSierra <8329080+MikePapaSierra@users.noreply.github.com>
2024-03-31 10:45:30 +0200

    feat(lsp): update config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,22 +1,19 @@
 local on_attach = function(_, bufnr)
 
-  local bufmap = function(keys, func)
-    vim.keymap.set('n', keys, func, { buffer = bufnr })
-  end
+  ---- TODO: Confilicting with Spectre keymapping
+  ----bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols)
+  ----bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols)
 
-  bufmap('<leader>rn', vim.lsp.buf.rename)
-  bufmap('<leader>ca', vim.lsp.buf.code_action)
-
-  bufmap('gd', vim.lsp.buf.definition)
-  bufmap('gD', vim.lsp.buf.declaration)
-  bufmap('gI', vim.lsp.buf.implementation)
-  bufmap('<leader>D', vim.lsp.buf.type_definition)
-
-  bufmap('gr', require('telescope.builtin').lsp_references)
-  bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols)
-  bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols)
-
-  bufmap('K', vim.lsp.buf.hover)
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP: [R]e[n]ame", buffer = bufnr })
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: [C]ode [A]ction", buffer = bufnr })
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "LSP: [G]oto [D]efinition", buffer = bufnr })
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "LSP: [G]oto [D]eclaration", buffer = bufnr })
+  vim.keymap.set("n", "td", vim.lsp.buf.type_definition, { desc = "LSP: [T]ype [D]efinition", buffer = bufnr })
+  vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, { desc = "LSP: [G]oto [R]eferences", buffer = bufnr })
+  vim.keymap.set("n", "gi", require("telescope.builtin").lsp_implementations, { desc = "LSP: [G]oto [I]mplementations", buffer = bufnr })
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP: Hover Documentation", buffer = bufnr })
+  vim.keymap.set("n", "<leader>k", vim.lsp.buf.signature_help, { desc = "LSP: Signature documentation", buffer = bufnr })
+  vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, { desc = "LSP: Signature documentation", buffer = bufnr })
 
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
@@ -55,6 +52,14 @@ require("mason-lspconfig").setup_handlers({
 			on_attach = on_attach,
 			capabilities = capabilities,
 			filetype = { "python" },
+		}
+	end,
+
+	["ansiblels"] = function()
+		require('lspconfig').ansible.setup {
+			on_attach = on_attach,
+			capabilities = capabilities,
+			filetype = { "yml", "yaml", "ansible" },
 		}
 	end
 })
