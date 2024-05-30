94252403f20473f1cfa60da66afdec3085fcdf04
kamikazestar <8329080+kamikazestar@users.noreply.github.com>
2024-05-30 10:32:06 +0200

    refactor(options.lua): encoding and smarttabs
    Added missing encoding option and enabled smarttabs due to the
    indentation issues. Added option to display title of the file.

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -8,6 +8,7 @@ vim.cmd('syntax enable') -- Setting is required by Vimwiki
 
 vim.o.wrap = false
 vim.o.encoding = 'utf-8'
+vim.scriptencoding = 'utf-8'
 vim.o.fileencoding = 'utf-8'
 vim.o.pumheight = 10
 vim.o.clipboard = 'unnamedplus'
@@ -17,16 +18,20 @@ vim.o.relativenumber = true
 
 vim.o.signcolumn = 'yes'
 
--- vim.o.smarttab = true
--- vim.o.expandtab = true
+vim.o.title = true
+
+vim.o.smarttab = true
+vim.o.expandtab = true
+vim.o.autoindent = true
 vim.o.smartindent = true
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.cursorline = true
 vim.o.laststatus = 2
+vim.o.breakindent = true
 
-vim.o.noflodenable = true
+-- vim.o.noflodenable = true
 vim.o.scrolloff = 10
 
 vim.o.updatetime = 300
