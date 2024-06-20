e3ccac9e1826234cea1d4528d40dbefde5af5dfa
kamikazestar <8329080+kamikazestar@users.noreply.github.com>
2024-06-20 23:08:10 +0200

    refactor(debbuging.lua) fix indentation

--- lua/plugins/debbuging.lua
+++ lua/plugins/debbuging.lua
@@ -1,40 +1,39 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"nvim-neotest/nvim-nio",
-		"rcarriga/nvim-dap-ui",
-		"leoluz/nvim-dap-go",
-	},
-	config = function()
-		local dap = require("dap")
-		local dapui = require("dapui")
-		-- require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "nvim-neotest/nvim-nio",
+        "rcarriga/nvim-dap-ui",
+        "leoluz/nvim-dap-go",
+    },
+    config = function()
+        local dap = require("dap")
+        local dapui = require("dapui")
+        -- require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')
 
-		require("dap-go").setup()
-		require("dapui").setup()
+        require("dap-go").setup()
+        require("dapui").setup()
 
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+        dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+        end
 
-		-- TODO: This is throwing an error
-		-- vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint(), {})
-		-- vim.keymap.set("n", "<Leader>dc", dap.continue(), {})
+        -- TODO: This is throwing an error
+        -- vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint(), {})
+        -- vim.keymap.set("n", "<Leader>dc", dap.continue(), {})
 
-		-- Catppuccin theme
-		local sign = vim.fn.sign_define
-		sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = ""})
-		sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = ""})
-		sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = ""})
-
-	end,
+        -- Catppuccin theme
+        local sign = vim.fn.sign_define
+        sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "" })
+        sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
+        sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = "" })
+    end,
 }
