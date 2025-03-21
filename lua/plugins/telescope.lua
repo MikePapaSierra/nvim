d91c9a867304bed427c11468e7aacae4cedc865c
kamikazestar <8329080+kamikazestar@users.noreply.github.com>
2025-03-22 00:02:38 +0100

    refactor(plugins): indentation changes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,25 +1,28 @@
 return {
 	{
-		'nvim-telescope/telescope.nvim',
-		branch = '0.1.x',
-		dependencies = { 'nvim-lua/plenary.nvim' }
+		"nvim-telescope/telescope.nvim",
+		lazy = true,
+		branch = "0.1.x",
+		dependencies = {
+			{ "nvim-lua/plenary.nvim" },
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+				cond = vim.fn.executable("make") == 1,
+			},
+			{
+				"nvim-telescope/telescope-ui-select.nvim",
+				config = function()
+					require("telescope").setup({
+						extensions = {
+							["ui-select"] = {
+								require("telescope.themes").get_dropdown({}),
+							},
+						},
+					})
+					require("telescope").load_extension("ui-select")
+				end,
+			},
+		},
 	},
-	{
-		'nvim-telescope/telescope-fzf-native.nvim',
-		build = 'make',
-		cond = vim.fn.executable 'make' == 1
-	},
-	{
-		'nvim-telescope/telescope-ui-select.nvim',
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown {}
-					}
-				}
-			})
-			require("telescope").load_extension("ui-select")
-		end
-	}
 }
