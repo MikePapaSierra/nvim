f0152c8f62defab90976d1a5d237b8f636debd97
MikePapaSierra <8329080+MikePapaSierra@users.noreply.github.com>
2025-03-21 23:54:37 +0100

    refactor(init): file cleanup

--- init.lua
+++ init.lua
@@ -1,17 +1,29 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+-- Bootstrap Lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup('plugins')
+-- Set leader key (this must be done before initializing lazy)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Initialize Lazy with dynamic loading of anything in plugins directory
+require("lazy").setup("plugins", {
+	change_detection = {
+		enabled = true, -- automatically chack for config file changes and reload ui
+		notify = false, -- turn off notifications whenever plugin config was changed
+	},
+})
+
 -- Modules not loaded by Lazy
 require("core.options")
 require("core.keymaps")
