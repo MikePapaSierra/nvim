08a3e6e6444b7edd0db26a698532eea213446fc4
kamikazestar <8329080+kamikazestar@users.noreply.github.com>
2025-03-21 23:56:09 +0100

    refactor(lualine): set lazymode to false

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,12 +1,13 @@
 return {
-	'nvim-lualine/lualine.nvim',
-	dependencies = { 
-		'nvim-tree/nvim-web-devicons'
+	"nvim-lualine/lualine.nvim",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		require('lualine').setup({
+		require("lualine").setup({
 			icons_enabled = true,
-			theme = 'catppuccin',
+			theme = "catppuccin",
 		})
-end,
+	end,
 }
