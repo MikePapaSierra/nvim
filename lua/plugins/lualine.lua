99201e1bc92e52660ab87b5207345d2bf209a456
MikePapaSierra <8329080+MikePapaSierra@users.noreply.github.com>
2025-08-25 00:17:54 +0200

    feat(lualine.lua): add priority

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,13 +1,14 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	lazy = false,
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		require("lualine").setup({
-			icons_enabled = true,
-			theme = "catppuccin",
-		})
-	end,
+    "nvim-lualine/lualine.nvim",
+    lazy = false,
+    priority = 1000,
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        require("lualine").setup({
+            icons_enabled = true,
+            theme = "catppuccin",
+        })
+    end,
 }
