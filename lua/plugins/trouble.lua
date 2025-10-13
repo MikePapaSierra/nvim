33df3b4404681e6319a36014fc97e30d95c82c31
MikePapaSierra <8329080+MikePapaSierra@users.noreply.github.com>
2025-10-13 06:33:21 +0200

    refactor(trouble): configuration cleanup

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,7 +1,20 @@
 return {
 	"folke/trouble.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-      require("trouble").setup {}
-    end
+	cmd = "Trouble",
+	keys = {
+		{ "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+		{ "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+		{ "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)" },
+		{ "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP Definitions / references / ... (Trouble)" },
+		{ "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+		{ "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
+		{ "gR", "<cmd>Trouble lsp_references toggle<cr>", desc = "LSP References (Trouble)" },
+	},
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("trouble").setup({
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+		})
+	end
 }
